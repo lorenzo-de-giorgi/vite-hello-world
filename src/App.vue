5847a86e6a87a8e72305b4fa8469df028868f1c7
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-04-09 14:12:24 +0200

    exercise completed

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <AppTitleComponent />
+  </div>
+</template>
+
+<script>
+  import AppTitleComponent from './components/AppTitleComponent.vue'
+  export default {
+    name:'App',
+    components: {
+      AppTitleComponent
+    },
+    data() {
+      return {
+        
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppTitleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'AppTitle',
+        data() {
+            return {
+                title: 'La mia prima app con Vite!'
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
